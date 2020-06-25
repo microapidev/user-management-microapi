6076c018b58b7de529a012b88da1352332520a9f
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-06-25 21:11:39 +0100

    feat: get self and delete self

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,27 @@
-const jwtUtil = require('jsonwebtoken')
-const serviceUser = require('../models/service_user')
+const jwtUtil = require("jsonwebtoken");
+const serviceUser = require("../models/service_user");
+const User = require("../models/user");
 
-const auth = async (req,res,next) => {
-    try {
-        const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwtUtil.verify(token,process.env.JWT_SECRET)
-        const user = await serviceUser.findOne({ email: decoded.email })
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwtUtil.verify(token, process.env.JWT_SECRET);
+    const user = await serviceUser.findOne({ email: decoded.email });
 
-        if (!user){
-            throw new Error('')
-        }
-        next()
-    }catch (e) {
-        res.status(401).json({status:'UnAuthorized', message: 'Request Not permitted', data:null})
+    if (!user) {
+      throw new Error("");
     }
-}
+    req.user = await User.findOne({ email: decoded.email });
+    next();
+  } catch (e) {
+    res
+      .status(401)
+      .json({
+        status: "UnAuthorized",
+        message: "Request Not permitted",
+        data: null,
+      });
+  }
+};
 
-module.exports = auth
+module.exports = auth;
